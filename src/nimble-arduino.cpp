50cd569d040934d2838b0405d0c05f2ebaa9fcde
CW-B-W <cw.brad.wang@gmail.com>
2023-07-20 02:11:19 +0800

    Add GATT R/W callbacks

--- src/nimble-arduino.cpp
+++ src/nimble-arduino.cpp
@@ -3,6 +3,40 @@
 #include "transport/uart/ble_hci_uart.h"
 
 #include "NimBLEDevice.h"
+#include "NimBLECharacteristic.h"
+
+class BEEFCallbacks: public BLECharacteristicCallbacks
+{
+    void onRead(NimBLECharacteristic* pCharacteristic)
+    {
+       /*
+        *   Must use a variable to store pCharacteristic->getValue()
+        *   Otherwise it may be overwrite by other local variables
+        */
+        NimBLEAttValue attval = pCharacteristic->getValue();
+        const char *val = attval.c_str();
+        ESP_LOGI("BEEFCallback::onRead()", "val = %s", val);
+
+       /* 
+        *   This is the wrong example, the value printed is incorrect
+        *   I speculate it's because `pCharacteristic->getValue()`
+        *   is not stored in a variable, after
+        *   `pCharacteristic->getValue().c_str()` is executed
+        *   it's automatically freed.
+        *   Therefore when `ESP_LOGI(...)` is executed, it overwrites
+        *   the content in ``pCharacteristic->getValue().c_str()`
+        */
+        // const char *val = pCharacteristic->getValue().c_str();
+        // ESP_LOGI("BEEFCallback::onRead()", "val = %s", val);
+    }
+
+    void onWrite(NimBLECharacteristic* pCharacteristic)
+    {
+        NimBLEAttValue attval = pCharacteristic->getValue();
+        const char *val = attval.c_str();
+        ESP_LOGI("BEEFCallback::onWrite()", "val = %s", val);
+    }
+};
 
 extern "C" void app_main()
 {
@@ -16,6 +50,7 @@ extern "C" void app_main()
             NIMBLE_PROPERTY::READ |
             NIMBLE_PROPERTY::WRITE
     );
+    pBeefCharacteristic->setCallbacks(new BEEFCallbacks());
     pBeefCharacteristic->setValue("Burger");
     pDeadService->start();
 
